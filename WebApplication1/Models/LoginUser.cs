db90ab7065d3008201ae4cedce8624cd0efaaab7
Daniel <trbaradaniel@icloud.com>
2025-10-29 11:07:55 +0100

    feat: Datenbank

---
+++ WebApplication1/Models/LoginUser.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1.Models;
+
+public class LoginUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data; // für AppDbContext
+
 var builder = WebApplication.CreateBuilder(args);
+
+// --- SQLite-DB einbinden ---
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(cs));
+// ----------------------------
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,17 +17,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
+// --- Routing ---
 app.MapControllerRoute(
         name: "areas",
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}")
@@ -30,5 +37,12 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// --- Automatisch DB erstellen / aktualisieren ---
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+// ------------------------------------------------
 
 app.Run();
